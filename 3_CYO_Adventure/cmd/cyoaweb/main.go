b6814d8d1a6cefa63531d3e09eedaef1802cbdd6
David Brungardt <brungardtdb@gmail.com>
2021-07-24 17:45:15 -0600

    Finished Routing
    Finished routing for different web pages

--- 3_CYO_Adventure/cmd/cyoaweb/main.go
+++ 3_CYO_Adventure/cmd/cyoaweb/main.go
@@ -1,36 +1,39 @@
 package main
 
 import (
-	// "flag"
-	// "fmt"
-	// "os"
-	// "path/filepath"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"log"
+	"net/http"
 
-	// "GOPHERCISES/3_CYO_Adventure/cyoa"
-	"GOPHERCISES/3_CYO_Adventure/cmd/cyoaweb/handler"
+	"GOPHERCISES/3_CYO_Adventure/cyoa"
 )
 
 func main() {
 
-	// absPath, err := filepath.Abs("../../gopher.json")
-	// check(err)
-	// fileName := flag.String("file", absPath, "the JSON file containing the CYOA story")
-	// flag.Parse()
-	// fmt.Printf("Using the story in %s.\n", *fileName)
+	absPath, err := filepath.Abs("../../gopher.json")
+	check(err)
+	portName := flag.Int("port", 8080, "the port to start the server on")
+	fileName := flag.String("file", absPath, "the JSON file containing the CYOA story")
+	flag.Parse()
+	fmt.Printf("Using the story in %s.\n", *fileName)
 
-	// f, err := os.Open(*fileName)
-	// check(err)
+	f, err := os.Open(*fileName)
+	check(err)
 
-	// story, err := cyoa.JsonStory(f)
-	// check(err)
+	story, err := cyoa.JsonStory(f)
+	check(err)
 
-	// fmt.Printf("%v\n", story)
-
-	handler.Run()
+	h := cyoa.NewHandler(story)
+	port := fmt.Sprintf(":%v", *portName)
+	fmt.Println("Starting the server on port ", port)
+	log.Fatal(http.ListenAndServe(port, h))
 }
 
-// func check(err error) {
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// }
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
--- 3_CYO_Adventure/cyoa/cyoa.go
+++ 3_CYO_Adventure/cyoa/cyoa.go
@@ -3,7 +3,98 @@ package cyoa
 import (
 	"encoding/json"
 	"io"
+	"net/http"
+	"html/template"
+	"strings"
+	"log"
 )
+
+type Story map[string]Chapter
+
+type Chapter struct {
+	Title      string   `json:"title"`
+	Paragraphs []string `json:"paragraphs"`
+	Options    []Option `json:"options"`
+}
+
+type Option struct {
+	Text    string `json:"text"`
+	Arc string `json:"arc"`
+}
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.New("").Parse(defaultHandlerTmplt))
+}
+
+var defaultHandlerTmplt = `
+<!DOCTYPE>
+<html>
+    <head>
+        <meta charset="UTF8">
+        <title>Choose Your Own Adventure</title>
+    </head>
+
+    <body>
+	<section class="page">
+        <h1>{{.Title}}</h1>
+        {{range .Paragraphs}}
+            <p>{{.}}</p>
+        {{end}}
+
+        <ul>
+        {{range .Options}}
+            <li>
+                <a href="/{{.Arc}}">{{.Text}}</a>
+            </li>
+        {{end}}
+        </ul>
+		</section>
+		<style>
+		body {
+		  font-family: helvetica, arial;
+		}
+		h1 {
+		  text-align:center;
+		  position:relative;
+		}
+		.page {
+		  width: 80%;
+		  max-width: 500px;
+		  margin: auto;
+		  margin-top: 40px;
+		  margin-bottom: 40px;
+		  padding: 80px;
+		  background: #FFFCF6;
+		  border: 1px solid #eee;
+		  box-shadow: 0 10px 6px -6px #777;
+		}
+		ul {
+		  border-top: 1px dotted #ccc;
+		  padding: 10px 0 0 0;
+		  -webkit-padding-start: 0;
+		}
+		li {
+		  padding-top: 10px;
+		}
+		a,
+		a:visited {
+		  text-decoration: none;
+		  color: #6295b5;
+		}
+		a:active,
+		a:hover {
+		  color: #7792a2;
+		}
+		p {
+		  text-indent: 1em;
+		}
+	  </style>
+
+    </body>
+</html>
+`
 
 func JsonStory(r io.Reader) (Story, error) {
 
@@ -16,15 +107,29 @@ func JsonStory(r io.Reader) (Story, error) {
 	return story, nil
 }
 
-type Story map[string]Chapter
-
-type Chapter struct {
-	Title      string   `json:"title"`
-	Paragraphs []string `json:"paragraphs"`
-	Options    []Option `json:"options"`
+type handler struct {
+	s Story
 }
 
-type Option struct {
-	Text    string `json:"text"`
-	Chapter string `json:"chapter"`
+func NewHandler(s Story) http.Handler {
+	return handler{s}
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "text/html")
+	path := strings.TrimSpace(r.URL.Path)
+	if path == "" || path == "/" {
+		path = "/intro"
+	}
+	path = path[1:]
+
+	if chapter, ok := h.s[path]; ok {
+		if err := tpl.Execute(w, chapter); err != nil {
+			log.Printf("%v", err)
+			http.Error(w, "Something went wrong....", http.StatusInternalServerError)
+		}
+		return
+	}
+	http.Error(w, "Are you lost?", http.StatusNotFound)
 }
